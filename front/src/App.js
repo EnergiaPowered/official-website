1401aed8541a39934695f885f6a92d0ff71d5391
mohamedsaad <mahameds19999@gmail.com>
2019-10-27 19:13:55 +0200

    add routing to front

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar";
+
+// routing components
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+// pages
+import HomePage from "./components/HomePage";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ HomePage } />
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
     </div>
   );
 }
