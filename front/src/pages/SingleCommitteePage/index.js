721ad12e35219105a7dc629774a9854bd6cacc44
mohamedsaad <mahameds19999@gmail.com>
2019-11-09 13:08:32 +0200

    refactor acoording to style guide

--- front/src/pages/SingleCommitteePage/index.js
+++ front/src/pages/SingleCommitteePage/index.js
@@ -14,37 +14,47 @@ import comm_mis_vis from "../../static_data/comm_extended.json";
 import "./style.css";
 
 export default function SingleCommittee(props) {
-    const data = comm_simp[props.match.params.id];
-    const misVis = comm_mis_vis[props.match.params.id];
+  const data = comm_simp[props.match.params.id];
+  const misVis = comm_mis_vis[props.match.params.id];
 
-    //This is just for scrolling to the top of the page
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    });
+  //This is just for scrolling to the top of the page
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
-    return (
-        <>
-            <Helmet>
-                <title>Energia Powered | {data.title}</title>
-            </Helmet>
-            <article id="SingleComm">
-                <header className="full-screen">
-                    <h1>{data.title}</h1>
-                    <div id="comm-icon">
-                        <FontAwesomeIcon icon={data.icon_class} />
-                    </div>
-                </header>
-                <section className="full-screen">
-                    <h2 className="text-center">Our Vision</h2>
-                    <p>{misVis.vision}</p>
-                    <hr />
-                    <h2 className="text-center">Our Mission</h2>
-                    <p>{misVis.mission}</p>
-                </section>
-            </article>
-            <div className="border-up">
-                <Footer />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>Energia Powered | {data.title}</title>
+      </Helmet>
+      <article className="page-component" id="SingleComm">
+        <header className="header-section">
+          <h1>{data.title}</h1>
+          <div id="comm-icon">
+            <FontAwesomeIcon icon={data.icon_class} />
+          </div>
+        </header>
+
+        <main>
+          <div className="container">
+            <section className="bg-section component-font">
+              <h2 className="text-center section-title">Our Vision</h2>
+              <p>{misVis.vision}</p>
+            </section>
+
+            <hr />
+
+            <section className="bg-section component-font">
+              <h2 className="section-title text-center">Our Mission</h2>
+              <p>{misVis.mission}</p>
+            </section>
+          </div>
+        </main>
+
+        <div className="border-up">
+          <Footer />
+        </div>
+      </article>
+    </>
+  );
 }
