10221593bac64ee94eea3bc6018c1e265102fcc5
mohamedsaad <mahameds19999@gmail.com>
2019-11-10 11:53:42 +0200

    finish not found page

--- front/src/pages/NotFound/index.js
+++ front/src/pages/NotFound/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import Particles from "react-particles-js";
+
+import "./style.css";
 
 export default () => {
   return (
-    <div>
-      Not Found
+    <div className="found-container page-component">
+      <Particles
+        height="100%"
+        className="particles"
+        params={{
+          particles: {
+            line_linked: {
+              color: "#FFFFFF"
+            },
+            number: {
+              value: 80
+            },
+            size: {
+              value: 2
+            }
+          },
+          interactivity: {
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse"
+              },
+              onresize: {
+                enable: true,
+                density_auto: true,
+                density_area: 100
+              }
+            }
+          }
+        }}
+      />
+
+      <section className="text-center found-content">
+        <div className="found-children">
+          <h1 className="section-title found-title"> Oops! </h1>
+          <p>The page you're looking for is Not Found</p>
+
+          <Link to="/"> Back to home </Link>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
