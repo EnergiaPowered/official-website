786b6b01ad91eee8406ae3bbd5879dc057df1393
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2019-11-21 23:50:24 +0200

    Fix 2: Adding validation/sanitization to the /message route module using Express Validator

--- back/index.js
+++ back/index.js
@@ -7,9 +7,8 @@ const db = require("./mongo");
 db();
 
 // Importing Routes
-const contactInfo = require("./routes/contactInfo")
+const contactInfo = require("./routes/contactInfo");
 const message = require("./routes/message.js");
-
 
 // parse the body of the request
 app.use(express.json());
@@ -19,24 +18,26 @@ app.use(cors());
 
 // production configurations
 if (process.env.NODE_ENV === "production") {
-    // assets
-    app.use(express.static(path.join(__dirname, "public")));
+  // assets
+  app.use(express.static(path.join(__dirname, "public")));
 
-    // always send the index.html file to handle SPA
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "public/index.html"));
-    });
+  // always send the index.html file to handle SPA
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 }
+
+// Security
+app.disable("x-powered-by");
 
 // Home Route for testing
 app.get("/", (req, res) => {
-    res.send("Hello From Backend");
+  res.send("Hello From Backend");
 });
 
 // Router Middlewares
 app.use(contactInfo);
 app.use(message);
-
 
 // listen to specific port
 const port = process.env.PORT || 4000;
