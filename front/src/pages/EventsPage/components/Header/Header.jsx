b084da5e67a6335dc86a8409ce9511f0a4eb531d
mohamedsaad <mahameds19999@gmail.com>
2019-12-05 16:55:44 +0200

    edits

--- front/src/pages/EventsPage/components/Header/Header.jsx
+++ front/src/pages/EventsPage/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default () => {
   return (
     <section id="header" className="header-section">
       <header className="container">
-        <h1 className="header-title"> events </h1>
+        <h1 className="events header-title"> events </h1>
       </header>
     </section>
   );
