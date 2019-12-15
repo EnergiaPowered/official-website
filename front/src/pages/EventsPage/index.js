319ecec20dab49b93ec08a749ebc6068eba13e39
Mahmoud Hammad <hodaman2012@gmail.com>
2019-12-15 15:19:53 +0200

    clean some shitty code

--- front/src/pages/EventsPage/index.js
+++ front/src/pages/EventsPage/index.js
@@ -1,86 +1,25 @@
+//*** to do later ***
+
+// request data from server ...
+// sort events from neer to far
+// if events number is odd rearrange the card to look better
+
 import React, { Component } from "react";
 import "./index.css";
 import Header from "./components/Header/Header";
 import Events from "./components/Events/Events";
 import Footer from "../../components/Footer";
+import {getEvents} from "./services/fakeEvents"
 
 export default class EventsPage extends Component {
   state = {
-    events: [
-      {
-        _id: "awfiwuerqherygfiaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Energia Gates" },
-          { title: "Date", value: "9/12/2019" },
-          { title: "Status", value: 1 }
-        ]
-      },
-      {
-        _id: "awfzzzzzzxaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Filed Trips" },
-          { title: "Date", value: "9/10/2019" },
-          { title: "Status", value: 1 }
-        ]
-      },
-      {
-        _id: "awccvvvvfaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Filed Trips" },
-          { title: "Date", value: "9/10/2019" },
-          { title: "Status", value: 1 }
-        ]
-      },
-      {
-        _id: "awfkjiooaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Filed Trips" },
-          { title: "Date", value: "9/10/2019" },
-          { title: "Status", value: 0 }
-        ]
-      },
-      {
-        _id: "awfewrgwaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Filed Trips" },
-          { title: "Date", value: "9/10/2019" },
-          { title: "Status", value: 0 }
-        ]
-      },
-      {
-        _id: "awr231gfaw",
-        image: {
-          url: "http://via.placeholder.com/640x360",
-          alt: "duck"
-        },
-        body: [
-          { title: " Name", value: "Filed Trips" },
-          { title: "Date", value: "9/10/2019" },
-          { title: "Status", value: 1 }
-        ]
-      }
-    ]
-  };
+    events:[]
+  }
+  componentDidMount() {
+    // validate data as you like
+    const events =getEvents()
+    this.setState({events})
+  }
   render() {
     return (
       <div id="EventsPage">
---
+++ front/src/pages/EventsPage/services/fakeEvents.js
@@ -0,0 +1,79 @@
+ const  events= [
+        {
+          _id: "awfiwuerqherygfiaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Energia Gates" },
+            { title: "Date", value: "9/12/2019" },
+            { title: "Status", value: 1 }
+          ]
+        },
+        {
+          _id: "awfzzzzzzxaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Filed Trips" },
+            { title: "Date", value: "9/10/2019" },
+            { title: "Status", value: 1 }
+          ]
+        },
+        {
+          _id: "awccvvvvfaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Filed Trips" },
+            { title: "Date", value: "9/10/2019" },
+            { title: "Status", value: 1 }
+          ]
+        },
+        {
+          _id: "awfkjiooaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Filed Trips" },
+            { title: "Date", value: "9/10/2019" },
+            { title: "Status", value: 0 }
+          ]
+        },
+        {
+          _id: "awfewrgwaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Filed Trips" },
+            { title: "Date", value: "9/10/2019" },
+            { title: "Status", value: 0 }
+          ]
+        },
+        {
+          _id: "awr231gfaw",
+          image: {
+            url: "http://via.placeholder.com/640x360",
+            alt: "duck"
+          },
+          body: [
+            { title: " Name", value: "Filed Trips" },
+            { title: "Date", value: "9/10/2019" },
+            { title: "Status", value: 1 }
+          ]
+        }
+      ]
+   
+
+    export  function getEvents(){
+        return events
+    }
