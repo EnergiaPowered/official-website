066be542ebe1e27a32c8aa99e42c72418d8cbde3
Ahmed Gamal <ahmedgamal.21724@gmail.com>
2021-01-17 02:31:06 +0200

    create event  template

--- front/src/modules/Events/EventsEP.js
+++ front/src/modules/Events/EventsEP.js
@@ -1,19 +1,40 @@
 import React from "react"
 import "./index.css";
 import Layout from './../../shared/Layout/index';
+import Competition from "./components/Competition";
+import { IoIosArrowDown } from 'react-icons/io';
+import Marathon from "./components/Marathon";
+import OnDayEvent from "./components/OnDayEvent";
+import Session from "./components/Session";
+
+
 
 function Events() {
 
     return (
-        <div className="page-component" >
+        <div className="page-component " >
             <Layout>
-                <div className="page_container">
-                <h1> Events</h1>
-
+                <div className="page_container  ">
+                        <h1 id="dropE">Events</h1>
+                    <div className="dropdown">
+                        <button className="dropbtn"><h1> <IoIosArrowDown/></h1></button>
+                        <div className="dropdown_content">
+                            <div className="value_dropDown" >Session</div>
+                            <div className="value_dropDown" >Marathon</div>
+                            <div className="value_dropDown" >OnDayEvent</div>
+                            <div className="value_dropDown" >Competition</div>
+                        </div>
+                    </div>
                 </div>
-                    
+    
             </Layout>
         </div>
     )
 }
-export default Events ;
+export default Events;
+
+
+{/* <Competition/>
+<Marathon/>
+<OnDayEvent/>
+<Session/> */}
---
+++ front/src/modules/Events/components/Session.js
@@ -0,0 +1,63 @@
+import React from "react"
+import "./indexComponents.css"
+import { HiLocationMarker } from 'react-icons/hi';
+import { MdDateRange } from 'react-icons/md';
+
+
+function Session() {
+
+    return (
+       
+ 
+            
+            <div className="container-fluid ">
+                <h2 className="titleE"> Session</h2>
+                       
+                        <div className="container_card1 row" >
+                            <div className="img_card " >
+                            <img src="https://via.placeholder.com/150?text=user-img" className="rounded-circle"/>
+                            </div>
+                            
+                            <div className="details_card">
+                                <div className="title_event"    > <h4>BE CREATIVE</h4></div>
+                                <div className="location_event" > <h6> <HiLocationMarker/> GREEK CAMPUS</h6></div>
+                                <div className="date_time_event"> <h6> <MdDateRange/>12/12</h6></div>
+                                <div className="type_event badge   badge-pill badge-primary"     > <h5>ClOSED</h5></div>  
+                            </div>
+                        </div>
+
+
+                        <div className="container_card2 row ">
+                            <div className="img_card">
+                            <img src="https://via.placeholder.com/150?text=user-img" className="rounded-circle"/>
+                        </div>
+                            <div className="details_card">
+                                <div className="title_event"    > <h4>BE CREATIVE</h4></div>
+                                <div className="location_event" > <h6> <HiLocationMarker/> GREEK CAMPUS</h6></div>
+                                <div className="date_time_event"> <h6> <MdDateRange/>12/12</h6></div>
+                                <div className="type_event badge   badge-pill badge-primary"     > <h5>OPEN</h5></div>
+                            </div>
+                        </div>
+
+
+
+                        <div className="container_card3 row ">
+                            <div className="img_card">
+                            <img src="https://via.placeholder.com/150?text=user-img" className="rounded-circle"/>
+                        </div>
+                            <div className="details_card">
+                                <div className="title_event"    > <h4>BE CREATIVE</h4></div>
+                                <div className="location_event" > <h6> <HiLocationMarker/> GREEK CAMPUS</h6></div>
+                                <div className="date_time_event"> <h6> <MdDateRange/>12/12</h6></div>
+                                <div className="type_event badge   badge-pill badge-primary"     > <h5>UPCOMING</h5></div>     
+                            </div>
+                        </div>
+
+
+                        </div>
+
+
+   
+    )
+}
+export default Session ;
