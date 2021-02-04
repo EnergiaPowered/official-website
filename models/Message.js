c444c935fc8b79a32383712808f02352b41c5347
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-02-04 05:16:46 +0200

    Testing production

--- models/Message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const messageSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    message: String
-});
-
-module.exports = mongoose.model("Message", messageSchema);
