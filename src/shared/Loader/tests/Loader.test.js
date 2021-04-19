d5fb15bdb732da6e2b28be451d5caadccf668c5a
salma27 <36279740+salma27@users.noreply.github.com>
2021-04-19 14:25:25 +0200

    check default title

--- src/shared/Loader/tests/Loader.test.js
+++ src/shared/Loader/tests/Loader.test.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import Loader from "./../../Loader/index";
 import { cleanup } from "@testing-library/react";
 import { create } from "react-test-renderer";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 // test for loader
 afterEach(cleanup);
@@ -15,4 +17,11 @@ it("renders without crashing", () => {
 it("matches snapshot", () => {
     const loader = create(<Loader />).toJSON();
     expect(loader).toMatchSnapshot();
+});
+Enzyme.configure({ adapter: new Adapter() });
+
+it("check default title", () => {
+    const loader = mount(<Loader />);
+    expect(loader.find("p").text()).toEqual("Loading...");
+    expect(loader.toJSON).toMatchSnapshot();
 });
